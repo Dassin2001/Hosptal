f44f7d318333d651ef553cb5f7e80a0b0ccbbd7c
Dassin2001 <fatimaamir012345@gmail.com>
2025-05-24 21:21:12 +0100

    Mapping des entites

--- src/main/java/umi/fs/hospital/entities/Medcin.java
+++ src/main/java/umi/fs/hospital/entities/Medcin.java
@@ -1,11 +1,22 @@
 package umi.fs.hospital.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Collection;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Medcin {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
     private String prenom;
     private String specialite;
-    private Collection<RandezVous>rendezVous;
+    @OneToMany(mappedBy = "medcin",fetch = FetchType.LAZY)
+    private Collection<RandezVous> rendezVous;
 }
--- src/main/java/umi/fs/hospital/entities/RandezVous.java
+++ src/main/java/umi/fs/hospital/entities/RandezVous.java
@@ -1,11 +1,27 @@
 package umi.fs.hospital.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RandezVous {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Temporal(TemporalType.DATE)
     private Date date;
-    private statusRDV status ;
+
+    private StatusRDV status ;
+    @ManyToOne
     private Patient patient;
+    @ManyToOne()
     private Medcin medcin;
+    @OneToOne(mappedBy = "randezVous")
+    private Consultation consultation;
 }
